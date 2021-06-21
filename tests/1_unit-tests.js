22676823cdfc8123842cc663c1ab40768daa114f
Oluwasegun Ajayi <ajayimooseize@gmail.com>
2021-06-21 12:00:57 +0100

    test 1

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,9 +1,17 @@
-const chai = require('chai');
+const chai = require("chai");
 let assert = chai.assert;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const ConvertHandler = require("../controllers/convertHandler.js");
 
 let convertHandler = new ConvertHandler();
 
-suite('Unit Tests', function(){
-
+suite("Unit Tests", function () {
+  test("Get Num", (done) => {
+    let input1 = "4L";
+    let input2 = "456L";
+    let input3 = "4.76L";
+    assert.equal(convertHandler.getNum(input1), 4);
+    assert.equal(convertHandler.getNum(input2), 456);
+    assert.equal(convertHandler.getNum(input3), 4.76);
+    done();
+  });
 });
