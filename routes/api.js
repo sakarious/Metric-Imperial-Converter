6d866872c133259bec9c593f32969c1d41967305
Oluwasegun Ajayi <ajayimooseize@gmail.com>
2021-06-21 11:42:15 +0100

    Initial Commit

---
+++ routes/api.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const expect = require("chai").expect;
+const ConvertHandler = require("../controllers/convertHandler.js");
+
+module.exports = function (app) {
+  //split_string = whole_string.split(/(\d+)/)
+  let convertHandler = new ConvertHandler();
+
+  app.get("/api/convert", (req, res) => {
+    if (req.query.input) {
+      let input = req.query.input;
+      let initNum = convertHandler.getNum(input);
+      let initUnit = convertHandler.getUnit(input);
+      let returnNum;
+      let returnUnit;
+      let toString;
+
+      let response = {};
+      response["initNum"] = initNum;
+      response["initUnit"] = initUnit;
+      response["returnNum"] = returnNum;
+      response["returnUnit"] = returnUnit;
+      response["toString"] = toString;
+      return res.json({ response });
+    }
+
+    return res.send('"input" query parameter is required');
+  });
+};
