4f00bafad878f8752cf1b5d2585cb22a652ab9f1
Oluwasegun Ajayi <ajayimooseize@gmail.com>
2021-06-21 20:32:44 +0100

    functional test

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -56,4 +56,19 @@ suite("Functional Tests", function () {
         done();
       });
   });
+
+  test("Convert kg - No number", function (done) {
+    chai
+      .request(server)
+      .get("/api/convert")
+      .query({ input: "kg" })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.body.initNum, 1);
+        assert.equal(res.body.initUnit, "kg");
+        assert.approximately(res.body.returnNum, 2.20462, 0.1);
+        assert.equal(res.body.returnUnit, "lbs");
+        done();
+      });
+  });
 });
