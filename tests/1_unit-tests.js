55066c376a813fff9d4afaee3eec9757c2842e57
Oluwasegun Ajayi <ajayimooseize@gmail.com>
2021-06-21 12:05:20 +0100

    test 2

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -14,4 +14,14 @@ suite("Unit Tests", function () {
     assert.equal(convertHandler.getNum(input3), 4.76);
     done();
   });
+
+  test("Get Unit", (done) => {
+    let input1 = "4L";
+    let input2 = "456KG";
+    let input3 = "4.76M";
+    assert.equal(convertHandler.getUnit(input1), 4);
+    assert.equal(convertHandler.getNu(input2), 456);
+    assert.equal(convertHandler.getNum(input3), 4.76);
+    done();
+  });
 });
